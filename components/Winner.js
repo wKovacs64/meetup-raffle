f349e2127b1e37057a8f7ee16757c442aabe5e7e
Justin Hall <justin.r.hall@gmail.com>
2017-12-30 16:24:24 -0700

    Justify winner name to the bottom of the winner box

--- components/Winner.js
+++ components/Winner.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Winner = ({ winner }) => (
   <a
-    className="dim link ba b--dark-blue dark-blue mb3 w4 shadow-5"
+    className="flex flex-column justify-between dim link ba b--dark-blue dark-blue mb3 w4 shadow-5"
     href={winner.profileURL}
     target="_blank"
     rel="noopener noreferrer"
