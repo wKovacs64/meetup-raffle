000c1e73bdeec6a2053f6ffb680a66ad979cb288
Justin Hall <justin.r.hall@gmail.com>
2017-12-30 23:35:17 -0700

    Embed stepper arrow SVGs

--- components/CountStepper.js
+++ components/CountStepper.js
@@ -12,10 +12,19 @@ const CountStepper = ({ inputId, field, form: { setFieldValue } }) => (
       <span className="flex flex-row">
         <button
           type="button"
-          className="bn bg-transparent h3 w3 pointer"
+          className="bn bg-transparent near-black h3 w3 pointer"
           {...getDecrementProps()}
         >
-          <img src="/static/down.svg" alt="decrement" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 40 40"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <g>
+              <path d="m22.5 17.5v-5h-5v5h-5l7.5 10 7.5-10h-5z" />
+            </g>
+          </svg>
         </button>
         <input
           id={inputId}
@@ -25,10 +34,19 @@ const CountStepper = ({ inputId, field, form: { setFieldValue } }) => (
         />
         <button
           type="button"
-          className="bn bg-transparent h3 w3 pointer"
+          className="bn bg-transparent near-black h3 w3 pointer"
           {...getIncrementProps()}
         >
-          <img src="/static/up.svg" alt="increment" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 40 40"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <g>
+              <path d="m20 12.5l-7.5 10h5v5h5v-5h5l-7.5-10z" />
+            </g>
+          </svg>
         </button>
       </span>
     )}
