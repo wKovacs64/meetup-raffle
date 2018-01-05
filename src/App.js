1513c34b30660954b5fe0bffffe3c3d9d288401e
Justin Hall <justin.r.hall@gmail.com>
2018-01-05 15:30:34 -0700

    Migrate project from next.js/now to create-react-app/netlify

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+import { Header, RaffleContainer } from './components';
+
+export default () => (
+  <Fragment>
+    <Header />
+    <RaffleContainer />
+  </Fragment>
+);
---
+++ src/components/Winner.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css, cx } from 'emotion';
+
+const Winner = ({ winner }) => {
+  const bgImg = css`
+    background-image: url(${winner.photoURL});
+  `;
+
+  return (
+    <a
+      className="flex flex-column dim link ba b--dark-blue dark-blue mb3 w4 shadow-5"
+      href={winner.profileURL}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <span className={cx('db bg-center cover h4', bgImg)} role="img" />
+      <div className="flex flex-grow-1 justify-center items-center bg-white">
+        <span className="tc pa3">{winner.name}</span>
+      </div>
+    </a>
+  );
+};
+
+Winner.propTypes = {
+  winner: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    photoURL: PropTypes.string.isRequired,
+    profileURL: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Winner;
