51b10ea312939d338b194783398ba013806807ae
Justin Hall <justin.r.hall@gmail.com>
2018-03-19 19:55:47 -0600

    Bring in react-testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-testing-library';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const { container } = render(<App />);
+  expect(container).toMatchSnapshot();
 });
