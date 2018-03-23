56d6760849f6e6f482fbb680740145ec1dfdb950
Justin Hall <justin.r.hall@gmail.com>
2018-03-22 22:51:51 -0600

    Remove async/await

--- src.functions/draw.js
+++ src.functions/draw.js
@@ -8,45 +8,21 @@ import {
   validateStatus,
 } from './helpers';
 
-const handler = async (request, context, callback) => {
+const handler = (request, context, callback) => {
   const headers =
     process.env.NODE_ENV === 'development'
       ? { 'Access-Control-Allow-Origin': '*' }
       : {};
 
+  let meetup;
+  let count;
+  let specificEventId;
+  let meetupApiKey;
+
   try {
-    const {
-      meetup,
-      count,
-      specificEventId,
-      meetupApiKey,
-    } = getParamsFromRequest(request);
-
-    const baseUrl = `https://api.meetup.com/${encodeURIComponent(
-      meetup,
-    )}/events`;
-    const eventUrlSuffix = '?status=upcoming&only=id,visibility';
-    const eventUrl = `${baseUrl}/${encodeURIComponent(
-      specificEventId,
-    )}${eventUrlSuffix}`;
-
-    const response = await axios.get(eventUrl, { validateStatus });
-    const eventId = getIdFromEvent(parseEventsResponse(response));
-
-    meetupRandomizer.setCustomApiUrl(
-      getRsvpsUrl(baseUrl, eventId, meetupApiKey),
-    );
-    const winners = await meetupRandomizer.run(meetup, eventId, count);
-
-    if (Array.isArray(winners) && winners.length) {
-      return callback(null, {
-        headers,
-        statusCode: 200,
-        body: JSON.stringify({ winners }),
-      });
-    }
-
-    throw new Error('Sorry, we received unexpected data for that request.');
+    ({ meetup, count, specificEventId, meetupApiKey } = getParamsFromRequest(
+      request,
+    ));
   } catch (err) {
     return callback(null, {
       headers,
@@ -54,6 +30,39 @@ const handler = async (request, context, callback) => {
       body: JSON.stringify({ error: { message: err.message } }),
     });
   }
+
+  const baseUrl = `https://api.meetup.com/${encodeURIComponent(meetup)}/events`;
+  const eventUrlSuffix = '?status=upcoming&only=id,visibility';
+  const eventUrl = `${baseUrl}/${encodeURIComponent(
+    specificEventId,
+  )}${eventUrlSuffix}`;
+
+  return axios
+    .get(eventUrl, { validateStatus })
+    .then(response => {
+      const eventId = getIdFromEvent(parseEventsResponse(response));
+      meetupRandomizer.setCustomApiUrl(
+        getRsvpsUrl(baseUrl, eventId, meetupApiKey),
+      );
+      return meetupRandomizer.run(meetup, eventId, count);
+    })
+    .then(winners => {
+      if (Array.isArray(winners) && winners.length) {
+        return callback(null, {
+          headers,
+          statusCode: 200,
+          body: JSON.stringify({ winners }),
+        });
+      }
+      throw new Error('Sorry, we received unexpected data for that request.');
+    })
+    .catch(err =>
+      callback(null, {
+        headers,
+        statusCode: 400,
+        body: JSON.stringify({ error: { message: err.message } }),
+      }),
+    );
 };
 
 export { handler };
