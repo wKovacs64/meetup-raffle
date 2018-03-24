09dab87b5dee340807cad25fec6d2476bb146b1d
Justin Hall <justin.r.hall@gmail.com>
2018-03-24 12:08:15 -0600

    Move client code into client directory

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'tachyons/css/tachyons.css';
 import 'core-js/fn/object/entries';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './client/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
