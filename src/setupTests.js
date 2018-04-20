e3a0e56ffea61324d0ce19fa4fb5cf6e8832f0f7
Justin Hall <justin.r.hall@gmail.com>
2018-04-20 09:55:13 -0600

    Serialize snapshots with jest-emotion for custom CSS validation

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import * as emotion from 'emotion';
+import { createSerializer } from 'jest-emotion';
+
+expect.addSnapshotSerializer(createSerializer(emotion));
 
 Enzyme.configure({ adapter: new Adapter() });
 
