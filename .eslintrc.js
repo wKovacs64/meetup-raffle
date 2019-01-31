b84b1b9fc72aa74b9a18a0787b39ae550f29e1d6
Justin Hall <justin.r.hall@gmail.com>
2019-01-31 11:12:20 -0700

    Bring in @wkovacs64/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,11 @@
 module.exports = {
-  env: {
-    browser: true,
-    jest: true,
-  },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  parser: 'babel-eslint',
+  extends: ['@wkovacs64/eslint-config/react'],
   plugins: ['emotion'],
   rules: {
     'emotion/jsx-import': 'error',
     'emotion/no-vanilla': 'error',
     'emotion/import-from-emotion': 'error',
     'emotion/styled-import': 'error',
-    'import/prefer-default-export': 'off',
     'jsx-a11y/label-has-for': [
       'error',
       {
@@ -22,18 +16,6 @@ module.exports = {
       'error',
       {
         controlComponents: ['Field'], // Formik
-      },
-    ],
-    'react/destructuring-assignment': 'off',
-    'react/jsx-filename-extension': 'off',
-    'valid-jsdoc': [
-      'error',
-      {
-        prefer: {
-          arg: 'param',
-          argument: 'param',
-          return: 'returns',
-        },
       },
     ],
   },
