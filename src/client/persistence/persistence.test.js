9e8cebb2506f51e2d58348ef98e7a3430e743ee0
Justin Hall <justin.r.hall@gmail.com>
2019-06-29 16:14:58 -0600

    refactor: convert RaffleContainer class to a function and hooks (#80)
    react@16.9.0-alpha.0 and react-dom@16.9.0-alpha.0 are included to eliminate annoying act() warnings
    in tests.

---
+++ src/client/persistence/persistence.test.js
@@ -0,0 +1,18 @@
+import { restore, preserve } from './persistence';
+
+const { localStorage } = global.window;
+
+test("persistence functions don't crash if localStorage is unavailable", () => {
+  // Remove localStorage temporarily
+  delete global.window.localStorage;
+  expect(global.window.localStorage).toBeUndefined();
+
+  expect(() => {
+    preserve({ key: 'value' });
+    restore('key');
+  }).not.toThrow();
+
+  // Restore localStorage
+  global.window.localStorage = localStorage;
+  expect(global.window.localStorage).toBeDefined();
+});
