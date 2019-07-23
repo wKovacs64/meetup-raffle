2f4b4f4f314d531aee77d9d49a29d6fb8425fb7c
Justin Hall <justin.r.hall@gmail.com>
2019-07-22 19:20:57 -0600

    style(lint): bring in eslint-plugin-wkovacs64

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
-  extends: ['@wkovacs64/eslint-config-react'],
+  extends: [
+    'plugin:wkovacs64/react',
+    'plugin:wkovacs64/jest',
+    'prettier',
+    'prettier/react',
+  ],
   plugins: ['emotion'],
   rules: {
     'emotion/jsx-import': 'error',
     'emotion/no-vanilla': 'error',
     'emotion/import-from-emotion': 'error',
     'emotion/styled-import': 'error',
-    'jsx-a11y/label-has-for': [
-      'error',
-      {
-        required: { every: ['id'] },
-      },
-    ],
     'jsx-a11y/label-has-associated-control': [
       'error',
       {
