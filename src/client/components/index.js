176a13bcc8b9cf050b4d8d7400a66b16b8a27c78
Justin Hall <justin.r.hall@gmail.com>
2020-05-16 12:55:29 -0600

    refactor(modules): remove components barrel file
    Some features of React only work on default exports/imports.

--- src/client/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as CountStepper } from './CountStepper';
-export { default as ErrorMessage } from './ErrorMessage';
-export { default as Header } from './Header';
-export { default as RaffleContainer } from './RaffleContainer';
-export { default as RaffleForm } from './RaffleForm';
-export { default as ResetButtons } from './ResetButtons';
-export { default as Results } from './Results';
-export { default as Winner } from './Winner';
