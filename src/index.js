48dd91328412cbfbba71d702dc765d4d1b74064b
Justin Hall <justin.r.hall@gmail.com>
2020-10-24 10:05:02 -0600

    chore(deps): bring in web-vitals with react-scripts v4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { loadDevTools } from './client/dev-tools/load';
 import App from './client/App';
 import registerServiceWorker from './registerServiceWorker';
+import reportWebVitals from './reportWebVitals';
 
 loadDevTools(() => {
   if (process.env.NODE_ENV !== 'production') {
@@ -19,4 +20,9 @@ loadDevTools(() => {
   );
 
   registerServiceWorker();
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
 });
