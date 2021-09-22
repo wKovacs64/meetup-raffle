cf17f9174e3237492ad3aa4aa1e41a001fc7a0e7
Justin Hall <justin.r.hall@gmail.com>
2021-09-22 16:09:49 -0600

    build(vite): replace @vitejs/plugin-react-refresh with @vitejs/plugin-react

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    reactRefresh(),
+    react({
+      jsxRuntime: 'classic',
+    }),
     VitePWA({
       manifest: {
         short_name: 'M. Raffle',
