2096dd0c1b96cc568c183995164aae00fbb7d3ad
Justin Hall <justin.r.hall@gmail.com>
2022-02-11 10:03:20 -0700

    chore: replace yarn with npm, update eslint to v8

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+// TODO: remove once eslint-plugin-wkovacs64 does this for us
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
   extends: [
     'plugin:wkovacs64/react',
