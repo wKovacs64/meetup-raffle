wKovacs64/meetup-raffle

commit 40a97086aa29e8fede609fc3c1cdb7b48448a4f5

refactor: prefer import.meta.env over process.env

src/functions/draw.js
@@ -7,10 +7,9 @@ import {
 } from './helpers';
 
 export async function handler(request /* , context */) {
-  const headers =
-    process.env.NODE_ENV === 'development'
-      ? /* c8 ignore next */ { 'Access-Control-Allow-Origin': '*' }
-      : {};
+  const headers = import.meta.env.DEV
+    ? /* c8 ignore next */ { 'Access-Control-Allow-Origin': '*' }
+    : {};
 
   let meetup;
   let count;

src/client/components/Raffle.jsx
@@ -182,7 +182,7 @@ const raffleMachine = createMachine(
 
 function Raffle() {
   const [state, send] = useMachine(raffleMachine, {
-    devTools: process.env.NODE_ENV === 'development',
+    devTools: import.meta.env.DEV,
   });
   const { meetup, winners, error, lastKnownGoodCount } = state.context;
 

src/index.jsx
@@ -7,7 +7,7 @@ import App from './client/App';
 import './index.css';
 
 loadDevTools(async () => {
-  if (process.env.NODE_ENV !== 'production') {
+  if (import.meta.env.DEV) {
     const { worker } = await import('./mocks/browser');
     worker.start();
   }

commit 208c52a8b2c56fa1738a1783cc49d4ab5b50c9fc

refactor: fix conditional CORS header logic for real
This was originally broken in commit
40a97086aa29e8fede609fc3c1cdb7b48448a4f5 and thought fixed by
8e940f2e77df41801a2b04a17dd1984771fdc16d, but I wasn't paying close
enough attention (Netlify functions are not built with Vite, _BUT_ we
are using Vitest to run the corresponding tests, so the previous attempt
at a fix appeared to work as the tests passed but didn't actually work
when running the app).

src/functions/draw.js
@@ -8,7 +8,7 @@ import {
 
 export async function handler(request /* , context */) {
   const headers =
-    import.meta.env.MODE === 'development'
+    process.env.NODE_ENV === 'development'
       ? /* c8 ignore next */ { 'Access-Control-Allow-Origin': '*' }
       : {};
 