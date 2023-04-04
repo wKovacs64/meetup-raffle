ddebdb0cfc86277d9115016d840df862fab7b39b
Justin Hall <justin.r.hall@gmail.com>
2023-04-04 12:27:31 -0600

    chore(deps): update Remix stuff for v2

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import { json, type LoaderArgs } from '@remix-run/node';
-import { useLoaderData, useTransition } from '@remix-run/react';
+import { useLoaderData, useNavigation } from '@remix-run/react';
 import { userSettingsCookie, type UserSettings } from '~/core/cookies.server';
 import LoadingSpinner from '~/raffle/loading-spinner';
 import RaffleForm from '~/raffle/raffle-form';
@@ -14,9 +14,9 @@ export const loader = async ({ request }: LoaderArgs) => {
 
 export default function HomePage() {
   const { userSettings } = useLoaderData<typeof loader>();
-  const transition = useTransition();
+  const navigation = useNavigation();
 
-  if (transition.state === 'loading' || transition.state === 'submitting') {
+  if (navigation.state === 'loading' || navigation.state === 'submitting') {
     return <LoadingSpinner />;
   }
 
