bd4733859a61e496ff614d816bcf89d6ded11583
Justin Hall <justin.r.hall@gmail.com>
2023-04-30 14:35:23 -0600

    chore: update a couple things

--- app/core/request-idle-callback-shim.ts
+++ app/core/request-idle-callback-shim.ts
@@ -1,8 +1,8 @@
 // TODO: remove once requestIdleCallback is available in Safari
 // https://caniuse.com/requestidlecallback
 export function requestIdleCallbackShim(cb: () => void) {
-  if ('requestIdleCallback' in window) {
-    return window.requestIdleCallback(cb);
+  if (typeof requestIdleCallback === 'function') {
+    return requestIdleCallback(cb);
   }
-  return window.setTimeout(cb, 1);
+  return setTimeout(cb, 1);
 }
