39b57e0fa263aca9d983758d9d6cace29a94ec26
Justin Hall <justin.r.hall@gmail.com>
2023-06-29 15:29:55 -0600

    chore: remove unused file
    This was missed in #351.

--- mocks/mocks-server.server.ts
+++
@@ -1,5 +0,0 @@
-import { setupServer } from 'msw/node';
-import { handlers } from './handlers';
-
-// setup Node request interception using the given mocks
-export const mocksServer = setupServer(...handlers);
