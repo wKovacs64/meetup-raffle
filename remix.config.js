7ed2051d0943675b60d3de45abe467ff66cc4991
Justin Hall <justin.r.hall@gmail.com>
2023-12-10 15:00:23 -0700

    chore(deps): update Netlify Remix adapter to v2
    This also converts the project to ESM.

--- remix.config.js
+++ remix.config.js
@@ -1,24 +1,19 @@
+import { config } from '@netlify/remix-adapter';
+
 /** @type {import('@remix-pwa/dev').WorkerConfig} */
-module.exports = {
+export default {
+  //
+  // Netlify Settings
+  //
+  ...(process.env.NODE_ENV === 'production' ? config : undefined),
   //
   // Remix Settings
   //
-  ignoredRouteFiles: ['**/.*'],
-  postcss: true,
-  server:
-    process.env.NETLIFY || process.env.NETLIFY_LOCAL
-      ? './server.ts'
-      : undefined,
-  serverBuildPath: '.netlify/functions-internal/server.js',
   serverDependenciesToBundle: [
     '@remix-pwa/cache',
     '@remix-pwa/sw',
     '@remix-pwa/strategy',
   ],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  serverModuleFormat: 'cjs',
   future: {},
   //
   // Remix PWA Settings
